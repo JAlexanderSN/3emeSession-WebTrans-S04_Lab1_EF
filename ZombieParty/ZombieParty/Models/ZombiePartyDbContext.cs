ec8a3ecda0334b65a31737d8c2459129bcced091
JAlexanderSN <2558467@cegepmontpetit.ca>
2026-09-08 14:58:27 -0400

    FCT Configuration EntityFramwork DbContext DI
    Création du ApplicationDbContext et ajout de DI du service SQL Server et de la connexion à la BD.

---
+++ ZombieParty/ZombieParty/Models/ZombiePartyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ZombieParty.Models
+{
+    public class ZombiePartyDbContext() : DbContext
+    {
+
+        public ZombiePartyDbContext(DbContextOptions<ZombiePartyDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+
+    }
+
+
+
+}
